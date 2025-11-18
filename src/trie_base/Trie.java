9827df422f78a91b8c90220c447b735599c6acf2
iljak2003 <iljak2006@mail.ru>
2025-11-18 20:02:03 +0300

    stared work on Trie, added insert method

--- src/trie_base/Trie.java
+++ src/trie_base/Trie.java
@@ -1,9 +1,23 @@
 package trie_base;
 
+import java.util.Locale;
+
 public class Trie {
 
     private TrieNode root = new TrieNode();
 
-
+    public void insert(String str) throws Exception {
+        str = str.toLowerCase(Locale.ROOT);
+        TrieNode node = root;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!node.hasChild(c)){
+                node.addChild(c);
+            }
+            node = node.getChild(c);
+        }
+        node.setKey(true);
+        node.addData();
+    }
 
 }
